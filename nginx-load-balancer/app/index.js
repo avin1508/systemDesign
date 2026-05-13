7cf9baaf7253129afdacf59a867a5c4a72e18cfb
avin1508 <omkumar55800@gmail.com>
2026-05-14 01:42:08 +0530

    built distributed rate limiting system using Redis, Docker Compose and NGINX

--- nginx-load-balancer/app/index.js
+++ nginx-load-balancer/app/index.js
@@ -1,23 +1,50 @@
 const express = require("express");
+const redis = require("redis");
 
 const app = express();
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
 
-    console.log(`Request handled by PID: ${process.pid}`);
-
-    // medium heavy work 😈
-    let total = 0;
-
-    for (let i = 0; i < 10; i++) {
-        total += i;
-    }
-
-    res.send(`Response from PID: ${process.pid}`);
+// Redis client 😈
+const client = redis.createClient({
+    url: "redis://redis:6379"
 });
 
+
+// Connect Redis 😈
+async function connectRedis() {
+
+    await client.connect();
+
+    console.log("Connected to Redis 😈");
+}
+
+connectRedis();
+
+
+// Route 😈
+app.get("/", async (req, res) => {
+
+    const ip = req.ip;
+
+    // atomic increment 😈
+    const requestCount = await client.incr(ip);
+
+    // first request 😈
+    if (requestCount === 1) {
+        await client.expire(ip, 60);
+    }
+
+    console.log(`IP: ${ip} → Count: ${requestCount}`);
+
+    // limit exceeded 😈
+    if (requestCount > 10) {
+        return res.status(429).send("Too many requests 😈");
+    }
+
+    res.send(`Allowed 😈 Count: ${requestCount}`);
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
